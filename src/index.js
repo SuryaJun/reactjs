12272e60f283404e497bb549a1b8c408c5bf0244
SuryaJun <suryanurj@gmail.com>
2022-04-23 14:50:28 +0700

    second commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-import App from './tugas/baasic_komponen/Counterstate';
-// import App from './App';
+import './index.css';
+// import App from './tugas/baasic_komponen/Counterstate';
+import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
